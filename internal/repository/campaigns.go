4edf0c1474ff84ac90bbadba9249ec4aa18fccbd
GunarsK <gunarskunakovs@gmail.com>
2026-02-15 10:40:47 +0200

    Upgrade to go v1.26 and fix db function names

--- internal/repository/campaigns.go
+++ internal/repository/campaigns.go
@@ -14,17 +14,17 @@ type CampaignRepository interface {
 }
 
 func (r *repository) GetCampaigns(ctx context.Context, auth AuthContext) (json.RawMessage, error) {
-	return r.callFunc(ctx, auth, "SELECT campaigns.get_campaigns()")
+	return r.callFunc(ctx, auth, "SELECT campaign.get_campaigns()")
 }
 
 func (r *repository) GetCampaign(ctx context.Context, auth AuthContext, id int64) (json.RawMessage, error) {
-	return r.callFunc(ctx, auth, "SELECT campaigns.get_campaign($1)", id)
+	return r.callFunc(ctx, auth, "SELECT campaign.get_campaign($1)", id)
 }
 
 func (r *repository) UpsertCampaign(ctx context.Context, auth AuthContext, data json.RawMessage) (json.RawMessage, error) {
-	return r.callFunc(ctx, auth, "SELECT campaigns.upsert_campaign($1::jsonb)", data)
+	return r.callFunc(ctx, auth, "SELECT campaign.upsert_campaign($1::jsonb)", data)
 }
 
 func (r *repository) DeleteCampaign(ctx context.Context, auth AuthContext, id int64) (bool, error) {
-	return r.execFunc(ctx, auth, "SELECT campaigns.delete_campaign($1)", id)
+	return r.execFunc(ctx, auth, "SELECT campaign.delete_campaign($1)", id)
 }
